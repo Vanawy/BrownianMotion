1eed2444ce19fd4423e83ae1753cfaf4c3824cb5
Vanawy <saharchuk999@gmail.com>
2019-05-21 00:19:47 +0300

    rip matter

--- particle.js
+++ particle.js
@@ -1,40 +1,68 @@
+
+// Constants
 const PARTICLE_RADIUS = 5;
 const PARTICLE_MASS = 10;
-const PARTICLE_MAX_SPEED = 50;
+const PARTICLE_MAX_SPEED = 1;
 const PARTICLE_COLOR = "#f14b7d";
 
 class Particle {
   get radius(){ return PARTICLE_RADIUS; }
   get mass(){ return PARTICLE_MASS; }
+  get vel(){ return this.body.velocity; }
+  get speed(){ return this.body.speed; }
   
-  constructor(cfg) {
+  constructor() {
     this.cfg = cfg;
-    this.pos = new createVector(random(width), random(height));
-    this.vel = new createVector(random(PARTICLE_MAX_SPEED), 0);
-    this.vel.rotate(random(TWO_PI));
+    let pos = new createVector(random(width), random(height));
+    // let vel = new createVector(random(PARTICLE_MAX_SPEED), 0);
+    let options = {
+      mass: PARTICLE_MASS,
+      frictionAir: 0,
+      friction: 0,
+      frictionStatic: 1,
+      inertia: Infinity,
+      restitution: 1,
+    };
+    this.body = Bodies.circle(pos.x, pos.y, PARTICLE_RADIUS, options);
+    World.add(world, this.body);
+
+    var vx = 0.1 * (Math.random() - 0.5)
+    var vy = 0.1 * (Math.random() - 0.5)
+    Engine._bodiesApplyGravity([this.body], { x: vx, y: vy })
+
+    // let vel = Vector.create(random(PARTICLE_MAX_SPEED));
+    // let angle = random(TWO_PI);
+    // vel = Vector.rotate(vel, angle);
+    
+    // Body.setVelocity(this.body, vel);
+    // Body.setAngle(this.body, angle);
   }
   
-  update(delta) {
-    this.pos.add(this.vel.copy().mult(delta));
-    if(this.pos.x > width || this.pos.x  < 0){
-      this.vel.x *= -1;
-      this.pos.x = this.pos.x < 0 ? 0 : width;
-    }
-    if(this.pos.y > height || this.pos.y  < 0){
-      this.vel.y *= -1;
-      this.pos.y = this.pos.y < 0 ? 0 : height;
-    }
-  }
+  // update(delta) {
+  //   this.pos.add(this.vel.copy().mult(delta));
+  //   if(this.pos.x > width || this.pos.x  < 0){
+  //     this.vel.x *= -1;
+  //     this.pos.x = this.pos.x < 0 ? 0 : width;
+  //   }
+  //   if(this.pos.y > height || this.pos.y  < 0){
+  //     this.vel.y *= -1;
+  //     this.pos.y = this.pos.y < 0 ? 0 : height;
+  //   }
+  // }
   
   draw() {
     push();
     noStroke();
     fill(PARTICLE_COLOR);
-    translate(this.pos.x, this.pos.y);
+
+    let pos = this.body.position;
+    let angle = this.body.angle;
+    translate(pos.x, pos.y);
+    rotate(angle);
     ellipse(0, 0,  PARTICLE_RADIUS * 2);
     stroke(0);
     if(this.cfg.showVelocity){
-      line(this.vel.x, this.vel.y , 0, 0);
+      line(this.body.velocity.x, this.body.velocity.y, 0, 0);
     }
     pop();
   }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+// Aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Vector = Matter.Vector,
+    Body = Matter.Body;
+
 let cfg = {
-  particlesCount: 100,
-  showVelocity: false,
-  showParticles: false,
+  particlesCount: 50,
+  showVelocity: true,
+  showParticles: true,
 };
 let brownian;
 let particles;
@@ -10,23 +18,61 @@ let cbParticles;
 let rsBg;
 let paragraphEnergy;
 
+let engine;
+let world;
+let lastDt = 100;
 
 function setup() {
+
   createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  world.gravity.y = 0.000001;
+  Engine.run(engine);
+  let options = {
+    isStatic: true,
+    frictionAir: 0,
+    friction: 0,
+    frictionStatic: 1,
+    inertia: Infinity,
+    restitution: 1,
+    mass: Infinity,
+  };
+  let walls = [
+    Bodies.rectangle(width / 2, -50, width + 100, 100, options),
+    Bodies.rectangle(width / 2, height + 50, width + 100, 100, options),
+    Bodies.rectangle(-50, height / 2, 100, height + 100, options),
+    Bodies.rectangle(width + 50, height / 2, 100, height + 100, options),
+  ];
+  World.add(world, walls);
+
   noStroke();
   angleMode(DEGREES);
-  particles = [];
-  direction = createVector(0, 0);
+  rectMode(CENTER);
   
   brownian = new Brownian(cfg);
+  particles = [];
   for(let i = 0; i < cfg.particlesCount; i++){
-    particles[i] = new Particle(cfg); 
+    particles[i] = new Particle(); 
   }
+
+    // create a renderer
+  var render = Render.create({
+      element: document.body,
+      engine: engine
+  });
+  render.options.showAngleIndicator = true;
+  render.options.showDebug = true;
+
+  // run the renderer
+  Render.run(render);
+
   createP('Configurations');
-  cbParticles = createCheckbox('show particles', true);
-  cbVelocity = createCheckbox('show velocity', false);
-  rsBg = createSlider(0, 255, 60);
-  paragraphEnergy = createP('Total energy = ');
+  cbParticles = createCheckbox('show particles', cfg.showParticles);
+  cbVelocity = createCheckbox('show velocity', cfg.showVelocity);
+  rsBg = createSlider(0, 255, 255);
+  paragraphEnergy = createP('');
+
 }
 
 function draw() {
@@ -34,14 +80,14 @@ function draw() {
   update(getDeltaTime());
   background(220, cfg.backgroundAlpha);
   
-  brownian.draw();
+  // brownian.draw();
   if(cfg.showParticles){
     particles.forEach((p, i) => {p.draw()});
   }
   drawFps();
-
-  let totalEnergy = brownian.mass * Math.pow(brownian.vel.mag(), 2) / 2;
-  particles.forEach((p, i) => {totalEnergy += p.mass * Math.pow(p.vel.mag(), 2) / 2});
+  let totalEnergy = 0;
+  // let totalEnergy = brownian.mass * Math.pow(brownian.vel.mag(), 2) / 2;
+  particles.forEach((p, i) => {totalEnergy += p.mass * Math.pow(p.speed, 2) / 2});
   paragraphEnergy.html('E = ' + totalEnergy.toFixed(2));
 }
 
@@ -52,19 +98,18 @@ function getInputs() {
 }
 
 function update(dt) {
-  particles.forEach((p, i) => {p.update(dt)});
-  particles.forEach((p, i) => {brownian.push(p)});
-  brownian.update(dt);
+  Engine.update(engine, dt);
+  lastDt = dt;
 }
 
 function getDeltaTime() {
-  return frameRate() != 0 ? max(1/frameRate(), 0.1) : 0.1;
+  return (frameRate() != 0 ? max(1/frameRate(), 0.1) : 0.1) * 1000;
 }
 
 function drawFps() {
   push();
   fill(0);
-  rect(0,0, 20, 10);
+  rect(10,5, 20, 10);
   fill('#0f0');
   textAlign(LEFT, TOP);
   text(""+frameRate().toFixed(0), 0, 0);
